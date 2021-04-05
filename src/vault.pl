41d25bb8d1d1117965d98996089be2a8ef53a98d
Aziz Sonawalla <sonawallaa@gmail.com>
2021-04-05 13:12:17 -0400

    update vault stubs

--- src/vault.pl
+++ src/vault.pl
@@ -3,10 +3,38 @@
 
 %% An encrypted vault %%
 
-% `Vault` is the vault from disk, decrypted using `Password`
-openVault(Password, Vault) :- 
+
+% The filename for storing encrypted vault data
+vaultFile("vault.txt").
+
+
+% The filename for storing hashed password
+passwordHashFile("passHash.txt").
+
+
+% True if the given values are the correct password for the vault
+% Vault requires all three values (key, nonce, tag) to encrypt/decrypt
+isCorrectPassword(password(Key, Nonce, Tag)) :- 
+    % TODO: Read password hash stored on disk
+    % TODO: Hash password(Key, Nonce, Tag)
+    % TODO: return true if the two hashes are equal
     notImplemented.
 
-% True if `Vault` is encrypted and saved to disk
-lockVault(Vault) :- 
+
+% `Vault` is the vault from disk, decrypted using the given password
+% `Vault` is a key-value dictionary (see dict.pl)
+% Assumes the password is correct (use isCorrectPassword first)
+openVault(password(Key, Nonce, Tag), Vault) :- 
+    % TODO: read encrypted data (as string) from disk
+    % TODO: decrypt data using `Password`
+    % TODO: build key-value dictionary from data
+    notImplemented.
+
+
+% True if `Vault` is encrypted and saved to disk using the given password
+% `Vault` is a key-value dictionary (see dict.pl)
+lockVault(password(Key, Nonce, Tag), Vault) :- 
+    % TODO: build string from key-value dictionary `Vault`
+    % TODO: encrypt string and store it in disk
+    % TODO: hash the password(Key, Nonce, Tag) and store it to disk
     notImplemented.
