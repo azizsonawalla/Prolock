3ea92218cf3e8f6e4793421b4578ac1617c61827
Aziz Sonawalla <sonawallaa@gmail.com>
2021-04-05 14:36:31 -0400

    add stub to perform actions

--- src/prolock.pl
+++ src/prolock.pl
@@ -27,6 +27,16 @@ existingUserWorkflow :-
     % TODO: perform vault actions (add, get, remove, exit)
     notImplemented.
 
+
+% Takes inputs from user and performs actions on the given vault
+performVaultActions(Vault) :- 
+    % TODO: Get command from user
+    % TODO: If command was to exit, lock vault and exit.
+    % TODO: Otherwise, perform command on given Vault (and generate new vault if required)
+    % TODO: Recursively call performVaultActions(NewVault)
+    notImplemented.
+
+
 % Entry-point for Prolock.
 % Either initiate newUserWorkflow or existingUserWorkflow
 main :- newUserWorkflow ; existingUserWorkflow.
