edb63a2c9755f9cfac24440ec8ba7c9f4549b2d2
Aziz Sonawalla <sonawallaa@gmail.com>
2021-04-07 09:12:15 -0400

    add integration test

---
+++ test/integration_test.pl
@@ -0,0 +1,19 @@
+:- begin_tests(integration).
+:- [src/disk].
+:- [src/crypto].
+:- [src/vault].
+
+% To run the tests:
+% ?- run_tests.
+
+test(encrypt_write_read_decrypt) :-
+    OriginalData = "this is a secret message",                % create a secret message
+    Key = "this is a password",                               % create a password
+    vaultFile(VaultFile),                                     % get the name of the vault file
+    encrypt(OriginalData, Key, Nonce, Tag, Encrypted),        % encrypt the secret message
+    writeData(Encrypted, VaultFile),                          % write the encrypted version to disk
+    readData(EncryptedFromDisk, VaultFile),                   % read the encrypted version back from disk
+    decrypt(Decrypted, Key, Nonce, Tag, EncryptedFromDisk),   % decrypt the version read from disk
+    Decrypted = OriginalData.                                 % check that the decrypted version is the same as the original
+
+:- end_tests(integration).
