4ca22814461f202f306d0213c9721975b182d863
Yifu Chen <45907512+chen-yifu@users.noreply.github.com>
2021-04-08 10:53:43 -0700

    Update src/dictionary.pl
    Co-authored-by: Aziz Sonawalla <35513492+azizsonawalla@users.noreply.github.com>

--- src/dictionary.pl
+++ src/dictionary.pl
@@ -35,7 +35,7 @@ insert(Key, Value, dict(K, V, D), dict(K, V, D2)) :- dif(Key, K), insert(Key, Va
 % If Dict does not have `Key`, then NewDict = Dict
 % TODO: implement this (1 hour) - Charles
 remove(Key, dict(Key, V, D), D).
-remove(Key, dict(K, V, D), dict(K, V, D2)) :- remove(Key, D, D2).
+remove(Key, dict(K, V, D), dict(K, V, D2)) :- dif(Key, K), remove(Key, D, D2).
 remove(Key, Dict, Dict) :- not(hasKey(Key, Dict)).  
 
 
