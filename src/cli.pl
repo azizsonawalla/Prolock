5b7d14ea49e314084a1a398aa4e7e26316de53fb
Matthew Hinz <mhinz@shaw.ca>
2021-04-09 11:20:05 -0700

    Update cli.pl

--- src/cli.pl
+++ src/cli.pl
@@ -127,4 +127,4 @@ isExitCommand(exit).
 
 % True if the user was shown the given dictionary
 % TODO: Implement this
-prettyPrintDict(Dict) :- notImplemented("prettyPrintDict").
+prettyPrintDict(Dict) :- notImplemented("cli -> prettyPrintDict").
